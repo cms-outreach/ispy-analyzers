9abb1dac8e50ba450fcb37c12a3a94ad5e80bf6c
Thomas Mc Cauley <sha1-9b8b7cc53491404f537ff22c65441882cbee992a@cern.ch>
2009-10-09 12:35:48 +0000

    write more info

--- src/ISpyTriggerEvent.cc
+++ src/ISpyTriggerEvent.cc
@@ -43,14 +43,13 @@ void ISpyTriggerEvent::analyze(const edm::Event& event, const edm::EventSetup& e
       "or remove the module that requires it";
   }
 
-  // FIXME: we should perhaps have a generic error class
+  IgDataStorage* storage = config->storage();
 
   if ( ! hltConfigProvided_ )
   {
     std::string error = 
       "### Error: ISpyTriggerEvent: cannot initialize HLTConfigProvider with process name "+processName_;
 
-    IgDataStorage* storage = config->storage();
     IgCollection& collection = storage->getCollection("Errors_V1");
     IgProperty ERROR_MSG = collection.addProperty("Error", std::string());
     IgCollectionItem item = collection.create();
@@ -73,7 +72,6 @@ void ISpyTriggerEvent::analyze(const edm::Event& event, const edm::EventSetup& e
                         + triggerEventTag_.instance() + ":"
                         + triggerEventTag_.process() + " not found.";
 
-    IgDataStorage* storage = config->storage();
     IgCollection& collection = storage->getCollection("Errors_V1");
     IgProperty ERROR_MSG = collection.addProperty("Error", std::string());
     IgCollectionItem item = collection.create();
@@ -88,140 +86,144 @@ void ISpyTriggerEvent::analyze(const edm::Event& event, const edm::EventSetup& e
                         + triggerResultsTag_.instance() + ":"
                         + triggerResultsTag_.process() + " not found.";
 
-    IgDataStorage* storage = config->storage();
     IgCollection& collection = storage->getCollection("Errors_V1");
     IgProperty ERROR_MSG = collection.addProperty("Error", std::string());
     IgCollectionItem item = collection.create();
     item[ERROR_MSG] = error;
   }
     
-  if ( triggerEvent.isValid() && triggerResults.isValid() )
-  {
-    IgDataStorage* storage = config->storage();
+  
 
-    std::string tevent = "TriggerEvent "
-                         + edm::TypeID (typeid (TriggerEvent)).friendlyClassName() + ":"
-                         + triggerEventTag_.label() + ":"
-                         + triggerEventTag_.instance() + ":"
-                         + triggerEventTag_.process();
+  std::string tevent = "TriggerEvent "
+                       + edm::TypeID (typeid (TriggerEvent)).friendlyClassName() + ":"
+                       + triggerEventTag_.label() + ":"
+                       + triggerEventTag_.instance() + ":"
+                       + triggerEventTag_.process();
         
-    std::string tresult = "TriggerResults "
-                          + edm::TypeID (typeid (TriggerResults)).friendlyClassName() + ":"
-                          + triggerResultsTag_.label() + ":"
-                          + triggerResultsTag_.instance() + ":"
-                          + triggerResultsTag_.process();
+  std::string tresult = "TriggerResults "
+                        + edm::TypeID (typeid (TriggerResults)).friendlyClassName() + ":"
+                        + triggerResultsTag_.label() + ":"
+                        + triggerResultsTag_.instance() + ":"
+                        + triggerResultsTag_.process();
 
-    IgCollection& products = storage->getCollection("Products_V1");
-    IgProperty PROD = products.addProperty("Product", std::string ());
-    IgCollectionItem item = products.create();
-    item[PROD] = tevent+"  "+tresult;
+  IgCollection& products = storage->getCollection("Products_V1");
+  IgProperty PROD = products.addProperty("Product", std::string ());
+  IgCollectionItem item = products.create();
+  item[PROD] = tevent+"  "+tresult;
 
-    IgCollection& triggerObjects = storage->getCollection("TriggerObjects_V1");
-	
-    IgProperty KEY = triggerObjects.addProperty("key", int(0));
-    IgProperty ID = triggerObjects.addProperty("id", int(0));
-    IgProperty PT = triggerObjects.addProperty("pt", 0.0);
-    IgProperty ETA = triggerObjects.addProperty("eta", 0.0);
-    IgProperty PHI = triggerObjects.addProperty("phi", 0.0);
-    IgProperty MASS = triggerObjects.addProperty("mass", 0.0);
+  IgCollection& triggerObjects = storage->getCollection("TriggerObjects_V1");
 
-    const size_type nTO = triggerEvent->sizeObjects();
-    const TriggerObjectCollection& triggerObjectCollection(triggerEvent->getObjects());
-	
-    for ( size_type iT = 0; iT != nTO; ++iT )
-    {
-      IgCollectionItem t = triggerObjects.create();
-      
-      const TriggerObject& triggerObject(triggerObjectCollection[iT]);
-	    
-      t[KEY] = static_cast<int>(iT);
-      t[ID] = static_cast<int>(triggerObject.id());
-      t[PT] = static_cast<double>(triggerObject.pt());
-      t[ETA] = static_cast<double>(triggerObject.eta());
-      t[PHI] = static_cast<double>(triggerObject.phi());
-      t[MASS] = static_cast<double>(triggerObject.mass());
-    }
-	
-    IgCollection& triggerPaths = storage->getCollection("TriggerPaths_V1");
+  IgProperty PATH = triggerObjects.addProperty("path", std::string());
+  IgProperty SLOT = triggerObjects.addProperty("slot", int(0));
+  IgProperty ML = triggerObjects.addProperty("moduleLabel", std::string());
+  IgProperty MT = triggerObjects.addProperty("moduleType", std::string());
+  IgProperty ID = triggerObjects.addProperty("VID", int(0));
+  IgProperty KEY = triggerObjects.addProperty("KEY", int(0));
+  IgProperty TID = triggerObjects.addProperty("id", int(0));
+  IgProperty PT = triggerObjects.addProperty("pt", 0.0);
+  IgProperty ETA = triggerObjects.addProperty("eta", 0.0);
+  IgProperty PHI = triggerObjects.addProperty("phi", 0.0);
+  IgProperty MASS = triggerObjects.addProperty("mass", 0.0);
 
-    IgProperty NAME = triggerPaths.addProperty("Name", std::string());
-    IgProperty INDEX = triggerPaths.addProperty("Index", int(0));
-    IgProperty WR = triggerPaths.addProperty("WasRun", int(0));
-    IgProperty AC = triggerPaths.addProperty("Accept", int(0));
-    IgProperty ER = triggerPaths.addProperty("Error", int(0));
-    IgProperty OBS = triggerPaths.addProperty("Objects", std::string());
+  IgCollection& triggerPaths = storage->getCollection("TriggerPaths_V1");
 
-    const unsigned int n(hltConfig_.size());
+  IgProperty NAME = triggerPaths.addProperty("Name", std::string());
+  IgProperty INDEX = triggerPaths.addProperty("Index", int(0));
+  IgProperty WR = triggerPaths.addProperty("WasRun", int(0));
+  IgProperty AC = triggerPaths.addProperty("Accept", int(0));
+  IgProperty ER = triggerPaths.addProperty("Error", int(0));
+  IgProperty OBS = triggerPaths.addProperty("Objects", std::string());
+
+  // This is the number of triggers over which we iterate
+  const unsigned int n(hltConfig_.size());
     
-    if ( triggerResults->size() != hltConfig_.size() )
-    {   
-      std::string error = "### Error: ISpyTriggerEvent: TriggerResults.size() != HLTConfigProvider.size()";
-      IgDataStorage* storage = config->storage();
+  if ( triggerResults->size() != hltConfig_.size() )
+  {   
+    std::string error = "### Error: ISpyTriggerEvent: TriggerResults.size() != HLTConfigProvider.size()";
+    IgDataStorage* storage = config->storage();
+    IgCollection& collection = storage->getCollection("Errors_V1");
+    IgProperty ERROR_MSG = collection.addProperty("Error", std::string());
+    IgCollectionItem item = collection.create();
+    item[ERROR_MSG] = error;
+
+    return;
+  }
+   
+  for ( unsigned int i = 0; i != n; ++i )
+  {
+    IgCollectionItem tp = triggerPaths.create();
+      
+    std::string triggerName = hltConfig_.triggerName(i); 
+    unsigned int triggerIndex = hltConfig_.triggerIndex(triggerName);
+      
+    if ( triggerIndex >= n )
+    {
+      std::string error = "### Error: ISpyTriggerEvent: "+triggerName+" not found";
       IgCollection& collection = storage->getCollection("Errors_V1");
       IgProperty ERROR_MSG = collection.addProperty("Error", std::string());
       IgCollectionItem item = collection.create();
       item[ERROR_MSG] = error;
-
+       
       return;
     }
-    
-    for ( unsigned int i = 0; i != n; ++i )
+      
+    tp[NAME] = triggerName;
+    tp[INDEX] = triggerIndex;
+      
+    tp[WR] = static_cast<int>(triggerResults->wasrun(triggerIndex));
+    tp[AC] = static_cast<int>(triggerResults->accept(triggerIndex));
+    tp[ER] = static_cast<int>(triggerResults->error(triggerIndex));
+
+    const std::vector<std::string>& moduleLabels(hltConfig_.moduleLabels(triggerIndex));
+    const unsigned int moduleIndex(triggerResults->index(triggerIndex));  
+
+    std::stringstream ss;
+
+    for ( unsigned int j = 0; j <= moduleIndex; ++j ) 
     {
-      IgCollectionItem tp = triggerPaths.create();
+      const std::string&  moduleLabel(moduleLabels[j]);
+      const std::string  moduleType(hltConfig_.moduleType(moduleLabel));
       
-      std::string triggerName = hltConfig_.triggerName(i); 
-      unsigned int triggerIndex = hltConfig_.triggerIndex(triggerName);
-      
-      if ( triggerIndex >= n )
+      const unsigned int filterIndex(triggerEvent->filterIndex(InputTag(moduleLabel,"",processName_)));
+
+      if ( filterIndex < triggerEvent->sizeFilters() ) 
       {
-        std::string error = "### Error: ISpyTriggerEvent: "+triggerName+" not found";
-        IgDataStorage* storage = config->storage();
-        IgCollection& collection = storage->getCollection("Errors_V1");
-        IgProperty ERROR_MSG = collection.addProperty("Error", std::string());
-        IgCollectionItem item = collection.create();
-        item[ERROR_MSG] = error;
-       
-        return;
-      }
-      
-      tp[NAME] = triggerName;
-      tp[INDEX] = triggerIndex;
-      
-      tp[WR] = static_cast<int>(triggerResults->wasrun(triggerIndex));
-      tp[AC] = static_cast<int>(triggerResults->accept(triggerIndex));
-      tp[ER] = static_cast<int>(triggerResults->error(triggerIndex));
-
-      const std::vector<std::string>& moduleLabels(hltConfig_.moduleLabels(triggerIndex));
-      const unsigned int moduleIndex(triggerResults->index(triggerIndex));  
-
-      std::stringstream ss;
-
-      for ( unsigned int j = 0; j <= moduleIndex; ++j ) 
-      {
-        const std::string&  moduleLabel(moduleLabels[j]);
-        const std::string  moduleType(hltConfig_.moduleType(moduleLabel));
-
-        const unsigned int filterIndex(triggerEvent->filterIndex(InputTag(moduleLabel,"",processName_)));
-
-        if ( filterIndex < triggerEvent->sizeFilters() ) 
-        {
-          const Vids& VIDS = triggerEvent->filterIds(filterIndex);
-          const Keys& KEYS = triggerEvent->filterKeys(filterIndex);
-
-          const size_type nI = VIDS.size();
-          const size_type nK = KEYS.size();
+        const Vids& VIDS = triggerEvent->filterIds(filterIndex);
+        const Keys& KEYS = triggerEvent->filterKeys(filterIndex);
         
-          assert(nI==nK);
-          const size_type N(std::max(nI,nK));
+        const size_type nI = VIDS.size();
+        const size_type nK = KEYS.size();
+        
+        assert(nI==nK);
+        const size_type N(std::max(nI,nK));
 
-          for ( size_type k = 0; k != N; ++k ) 
-            ss << VIDS[k] << "/" << KEYS[k] <<" ";
-        }
+        const TriggerObjectCollection& triggerObjectCollection(triggerEvent->getObjects());
+          
+        for ( size_type k = 0; k != N; ++k )
+        {
+          ss << VIDS[k] << "/" << KEYS[k] <<" ";
+          
+          IgCollectionItem t = triggerObjects.create();
+          const TriggerObject& triggerObject(triggerObjectCollection[KEYS[k]]);
+	 
+          t[PATH] = triggerName;
+          t[SLOT] = j;
+          t[ML] = moduleLabel;
+          t[MT] = moduleType;
+          
+          t[ID] = static_cast<int>(VIDS[k]);
+          t[KEY] = static_cast<int>(KEYS[k]);
+  
+          t[TID] = static_cast<int>(triggerObject.id());
+          t[PT] = static_cast<double>(triggerObject.pt());
+          t[ETA] = static_cast<double>(triggerObject.eta());
+          t[PHI] = static_cast<double>(triggerObject.phi());
+          t[MASS] = static_cast<double>(triggerObject.mass());
+        }        
       }
-      
-      tp[OBS] = ss.str();
-
     }
+      
+    tp[OBS] = ss.str();
   }    
 }
 

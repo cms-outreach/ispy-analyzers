067d4c0e277013d0644236281e84dd65406e6d06
ispy <>
2010-07-21 05:04:58 +0000

    This commit was manufactured by cvs2git to create tag 'CMSSW_3_9_7'.
    Sprout from master 2010-07-21 05:04:31 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Convert BuildFiles to XML'
    Cherrypick from master 2010-03-15 10:49:07 UTC Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch> '* ISpyEvent.cc: Introduce local time. Evolve schema.':
        ChangeLog
        python/ISpy_Online_Producer_cff.py
        python/ispy_32X_cfg.py
    Delete:
        BuildFile
        interface/ISpyEBDigi.h
        interface/ISpyEEDigi.h
        interface/ISpyEventFeatures.h
        python/ISpyEBDigi_cfi.py
        python/ISpyEEDigi_cfi.py
        python/ISpyEventFeatures_cfi.py
        python/ispy_7TeV.sh
        python/ispy_EcalDigis_cfg.py
        python/ispy_EventFeatures_cfg.py
        src/ISpyEBDigi.cc
        src/ISpyEEDigi.cc
        src/ISpyEventFeatures.cc

--- python/ISpyEEDigi_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-ISpyEEDigi = cms.EDAnalyzer('ISpyEEDigi',
-                            iSpyEEDigiTag = cms.InputTag('simEcalDigis:eeDigis'),
-                            iSpyEERecHitTag = cms.InputTag('ecalRecHit:EcalRecHitsEE')
-                           )

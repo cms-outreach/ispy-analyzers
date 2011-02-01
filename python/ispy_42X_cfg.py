cffb3016e485ecd92ac27ebbd9ff3199be071ebf
Thomas Mc Cauley <sha1-9b8b7cc53491404f537ff22c65441882cbee992a@cern.ch>
2011-02-01 16:25:24 +0000

    test file for 42X

---
+++ python/ispy_42X_cfg.py
@@ -0,0 +1,32 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("ISPY")
+process.load("ISpy/Analyzers/ISpy_Producer_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+from Configuration.PyReleaseValidation.autoCond import autoCond
+process.GlobalTag.globaltag = autoCond['mc']
+
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring('/store/relval/CMSSW_4_2_0_pre1/RelValZTT/GEN-SIM-RECO/START311_V1-v1/0078/52FBF564-C22B-E011-919B-002354EF3BD2.root')
+                            )
+
+from FWCore.MessageLogger.MessageLogger_cfi import *
+
+process.add_(
+    cms.Service("ISpyService",
+    outputFileName = cms.untracked.string('RelValZTT.ig'),
+    outputIg = cms.untracked.bool(True),
+    outputMaxEvents = cms.untracked.int32(10),
+    online = cms.untracked.bool(False),
+    debug = cms.untracked.bool(False)
+    )
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+process.p1 = cms.Path(process.iSpy_sequence)

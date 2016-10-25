f0b6e6888ee207eae06243621a75ddfa667579ea
tpmccauley <thomas.mccauley@cern.ch>
2016-10-25 11:57:21 +0200

    fixes from tcox

--- src/ISpyCSCCorrelatedLCTDigi.cc
+++ src/ISpyCSCCorrelatedLCTDigi.cc
@@ -17,6 +17,8 @@
 
 #include "DataFormats/MuonDetId/interface/CSCDetId.h"
 
+#include <iostream>
+
 using namespace edm::service;
 using namespace edm;
 
@@ -63,7 +65,7 @@ void ISpyCSCCorrelatedLCTDigi::analyze(const edm::Event& event, const edm::Event
 			  + inputTag_.instance() + ":"
 			  + inputTag_.process();
 
-    IgCollection& products = storage->getCollection("Products_V2");
+    IgCollection& products = storage->getCollection("Products_V1");
     IgProperty PROD = products.addProperty("Product", std::string ());
     IgCollectionItem item = products.create();
     item[PROD] = product;
@@ -85,49 +87,84 @@ void ISpyCSCCorrelatedLCTDigi::analyze(const edm::Event& event, const edm::Event
 	dri != driEnd; ++dri )
     {
       const CSCDetId& cscDetId = (*dri).first;
+
       const CSCCorrelatedLCTDigiCollection::Range& range = (*dri).second;
 
-      for(CSCCorrelatedLCTDigiCollection::const_iterator dit = range.first;
-	  dit != range.second; ++dit)
+      for ( CSCCorrelatedLCTDigiCollection::const_iterator dit = range.first;
+	        dit != range.second; ++dit)
       {      
 	IgCollectionItem digi = digis.create();
-	const CSCLayer* layer = geom->chamber(cscDetId)->layer(3);
-	const CSCLayerGeometry *layerGeom = layer->geometry();
-	const float WGCenter = layerGeom->middleWireOfGroup( dit->getKeyWG() + 1 ); // BEWARE: LCT counts from 0
-	const int halfstrip_id = dit->getStrip(); // BEWARE: LCT counts from 0
 
-	// Since xOfStrip requires int strip we cannot use it for half-strip ids in the trigger primitive
-	// Instead, jump through (my own) hoops in order to get a precise local x for a half-strip value
+        // T. Cox
+        // 1) We can only identify ME1/1A or B once we know half-strip range 
+        // 2) Since xOfStrip requires int strip we cannot use it for half-strip ids in the trigger primitive,
+        // so jump through (my own) hoops in order to get a precise local x for a half-strip value
 
-        float fstrip = float( halfstrip_id + 0.5 )/2.; // 0->0.25, 1->0.75, 2->1.25, ... 159->79.75
-        LocalPoint lST = layerGeom->topology()->localPosition( MeasurementPoint( fstrip, 0.5 ) );
-        LocalPoint lSB = layerGeom->topology()->localPosition( MeasurementPoint( fstrip, -0.5 ) );
-	GlobalPoint gST = layer->toGlobal( lST );
-	GlobalPoint gSB = layer->toGlobal( lSB );
+	int halfstrip = dit->getStrip(); // BEWARE: LCT counts from 0
+        short iring = cscDetId.ring(); // for ME1/1A will need to reset from 1 to 4
 
-	std::pair< LocalPoint, LocalPoint > wP = layerGeom->wireTopology()->wireEnds( WGCenter );
-	GlobalPoint gW1 = layer->toGlobal( wP.first );
-	GlobalPoint gW2 = layer->toGlobal( wP.second );
+        GlobalPoint gST, gSB, gW1, gW2;
 
-	digi[POS1] = IgV3d(static_cast<double>(gST.x()/100.0),
-			   static_cast<double>(gST.y()/100.0),
-			   static_cast<double>(gST.z()/100.0));
-	digi[POS2] = IgV3d(static_cast<double>(gSB.x()/100.0),
-			   static_cast<double>(gSB.y()/100.0),
-			   static_cast<double>(gSB.z()/100.0));
+        bool me11a = false;
+        bool me11 = (cscDetId.station()==1) && (iring==1); // in ME1/1?
+        if ( me11 ) {
+          me11a = (halfstrip > 127 ); // 0-127 <-> 64 channels of ME1/1B; after that  48 channels of ME1/1A
+          if ( me11a ) {
+            iring = 4;
+            const CSCDetId id1 = CSCDetId( cscDetId.endcap(), cscDetId.station(), iring, cscDetId.chamber(), 3 ); // layer 3 id
+            const CSCLayer* layer1 = geom->layer( id1 );
+            const CSCLayerGeometry* layerGeom1 = layer1->geometry();
 
-	digi[POS3] = IgV3d(static_cast<double>(gW1.x()/100.0),
-			   static_cast<double>(gW1.y()/100.0),
-			   static_cast<double>(gW1.z()/100.0));
-	digi[POS4] = IgV3d(static_cast<double>(gW2.x()/100.0),
-			   static_cast<double>(gW2.y()/100.0),
-			   static_cast<double>(gW2.z()/100.0));
+            int hs1 = halfstrip - 128; // reset halfstrip from 128... to 0...
+            float fstrip = float( hs1 + 0.5 )/2.; // 0->0.25, 1->0.75, 2->1.25, ... 159->79.75
+            LocalPoint lST = layerGeom1->topology()->localPosition( MeasurementPoint( fstrip, 0.5 ) );
+            LocalPoint lSB = layerGeom1->topology()->localPosition( MeasurementPoint( fstrip, -0.5 ) );
+            gST = layer1->toGlobal( lST );
+            gSB = layer1->toGlobal( lSB );
 
-	digi[DETID] = static_cast<int>(cscDetId.rawId());
-	digi[EC] = static_cast<int>(cscDetId.endcap());
-	digi[ST] = static_cast<int>(cscDetId.station());
-	digi[RG] = static_cast<int>(cscDetId.ring());
-	digi[CH] = static_cast<int>(cscDetId.chamber());
+            float WGCenter = layerGeom1->middleWireOfGroup( dit->getKeyWG() + 1 ); // BEWARE: LCT counts from 0
+            std::pair< LocalPoint, LocalPoint > wP = layerGeom1->wireTopology()->wireEnds( WGCenter );
+            gW1 = layer1->toGlobal( wP.first );
+            gW2 = layer1->toGlobal( wP.second );
+
+          }
+        }
+        if ( ! me11a ) {
+          const CSCLayer* layer = geom->chamber( cscDetId )->layer( 3 );
+          const CSCLayerGeometry* layerGeom = layer->geometry();
+
+          float fstrip = float( halfstrip + 0.5 )/2.; // 0->0.25, 1->0.75, 2->1.25, ... 159->79.75
+          LocalPoint lST = layerGeom->topology()->localPosition( MeasurementPoint( fstrip, 0.5 ) );
+          LocalPoint lSB = layerGeom->topology()->localPosition( MeasurementPoint( fstrip, -0.5 ) );
+          gST = layer->toGlobal( lST );
+          gSB = layer->toGlobal( lSB );
+
+          float WGCenter = layerGeom->middleWireOfGroup( dit->getKeyWG() + 1 ); // BEWARE: LCT counts from 0
+          std::pair< LocalPoint, LocalPoint > wP = layerGeom->wireTopology()->wireEnds( WGCenter );
+          gW1 = layer->toGlobal( wP.first );
+          gW2 = layer->toGlobal( wP.second );
+
+	} 
+ 
+        digi[POS1] = IgV3d(static_cast<double>(gST.x()/100.0),
+                           static_cast<double>(gST.y()/100.0),
+                           static_cast<double>(gST.z()/100.0));
+        digi[POS2] = IgV3d(static_cast<double>(gSB.x()/100.0),
+                           static_cast<double>(gSB.y()/100.0),
+                           static_cast<double>(gSB.z()/100.0));
+
+        digi[POS3] = IgV3d(static_cast<double>(gW1.x()/100.0),
+                           static_cast<double>(gW1.y()/100.0),
+                           static_cast<double>(gW1.z()/100.0));
+        digi[POS4] = IgV3d(static_cast<double>(gW2.x()/100.0),
+                           static_cast<double>(gW2.y()/100.0),
+                           static_cast<double>(gW2.z()/100.0));
+
+        digi[DETID] = static_cast<int>(cscDetId.rawId());
+        digi[EC] = static_cast<int>(cscDetId.endcap());
+        digi[ST] = static_cast<int>(cscDetId.station());
+        digi[RG] = static_cast<int>( iring );
+        digi[CH] = static_cast<int>(cscDetId.chamber());
       }
     }
   }

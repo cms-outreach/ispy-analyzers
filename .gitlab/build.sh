bb5ad1bce4cd8aaff13549c5ec592c38d582099a
Tom McCauley <tpmccauley@gmail.com>
2020-10-21 11:43:43 +0100

    fixed

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -5,14 +5,15 @@ set -ex
 shopt -s expand_aliases
 export BUILD_DIR=${PWD}
 source ${CMS_PATH}/cmsset_default.sh
-# CMSSW_RELEASE doesn't propagate to here and is "unbound". Fix.
-cmsrel CMSSW_10_2_9
-cd CMSSW_10_2_9/src
+cd /home/cmsusr
+export CMSSW_RELEASE=CMSSW_10_2_9
+cmsrel ${CMSSW_RELEASE}
+cd ${CMSSW_RELEASE}/src
 git clone https://github.com/cms-outreach/ispy-services.git ISpy/Services
 cmsenv
 mkdir -p ISpy/Analyzers
-mv ${BUILD_DIR}/src CMSSW_10_2_9/src/ISpy/Analyzers/
-mv ${BUILD_DIR}/interface CMSSW_10_2_9/src/ISpy/Analyzers/
-mv ${BUILD_DIR}/python CMSSSW_10_2_9/src/ISpy/Analyzers/
-mv ${BUILD_DIR}/BuildFile.xml CMSSW_10_2_9/src/ISpy/Analyzers/
+mv ${BUILD_DIR}/src ISpy/Analyzers/
+mv ${BUILD_DIR}/interface ISpy/Analyzers/
+mv ${BUILD_DIR}/python ISpy/Analyzers/
+mv ${BUILD_DIR}/BuildFile.xml ISpy/Analyzers/
 scram b

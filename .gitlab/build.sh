e611209a6a228a10e7e94106ce4ace66ffa94f18
Tom McCauley <tpmccauley@gmail.com>
2020-10-21 10:44:36 +0100

    fix vars again

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -3,15 +3,17 @@
 set -ex
 
 shopt -s expand_aliases
-set +u && source ${CMS_PATH}/cmsset_default.sh; set -u
+export BUILD_DIR=${PWD}
+source ${CMS_PATH}/cmsset_default.sh
+cd /home/cmsusr
 # CMSSW_RELEASE doesn't propagate to here and is "unbound". Fix.
 cmsrel CMSSW_10_2_9
 cd CMSSW_10_2_9/src
 git clone https://github.com/cms-outreach/ispy-services.git ISpy/Services
 cmsenv
 mkdir -p ISpy/Analyzers
-mv ${CI_PROJECT_DIR}/src ${CMSSW_BASE}/src/ISpy/Analyzers/
-mv ${CI_PROJECT_DIR}/interface ${CMSSW_BASE}/src/ISpy/Analyzers/
-mv ${CI_PROJECT_DIR}/python ${CMSSW_BASE}/src/ISpy/Analyzers/
-mv ${CI_PROJECT_DIR}/BuildFile.xml ${CMSSW_BASE}/src/ISpy/Analyzers/
+mv ${BUILD_DIR}/src CMSSW_10_2_9/src/ISpy/Analyzers/
+mv ${BUILD_DIR}/interface CMSSW_10_2_9/src/ISpy/Analyzers/
+mv ${BUILD_DIR}/python CMSSSW_10_2_9/src/ISpy/Analyzers/
+mv ${BUILD_DIR}/BuildFile.xml CMSSW_10_2_9/src/ISpy/Analyzers/
 scram b

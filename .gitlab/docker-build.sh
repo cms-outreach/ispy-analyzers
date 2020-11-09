963cbf220bad5b6280b1e3062a0547ba3b0b724a
Tom McCauley <tpmccauley@gmail.com>
2020-11-09 11:54:20 +0000

    separate build and docker

--- .gitlab/docker-build.sh
+++ .gitlab/docker-build.sh
@@ -4,6 +4,9 @@ set -ex
 shopt -s expand_aliases
 export BUILD_DIR=${PWD}
 set +u && source ${CMS_PATH}/cmsset_default.sh; set -u
+ls -ltr
+cd /home/cmsusr
+export CMSSW_RELEASE=CMSSW_10_2_21
 cmsrel ${CMSSW_RELEASE}
 cd ${CMSSW_RELEASE}/src
 git clone https://github.com/cms-outreach/ispy-services.git ISpy/Services
@@ -12,5 +15,6 @@ mv ${BUILD_DIR}/src ISpy/Analyzers/
 mv ${BUILD_DIR}/interface ISpy/Analyzers/
 mv ${BUILD_DIR}/python ISpy/Analyzers/
 mv ${BUILD_DIR}/BuildFile.xml ISpy/Analyzers/
+ls -ltr
 cmsenv
 scram b -v

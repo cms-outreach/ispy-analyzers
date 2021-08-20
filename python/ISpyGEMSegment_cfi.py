7919405ae70b7eb7b80f36bd700b528144d9ce47
Tom McCauley <tpmccauley@gmail.com>
2021-08-20 09:58:36 -0400

    rm GEM stuff

--- python/ISpyGEMSegment_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-ISpyGEMSegment = cms.EDAnalyzer('ISpyGEMSegment' ,
-                                iSpyGEMSegmentTag = cms.InputTag("gemSegments")
-                                )

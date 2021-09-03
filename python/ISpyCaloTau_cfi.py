4353846fdbb41e2646a22d8bc8d4dcf6673b4ff5
mccauley <thomas.mccauley@cern.ch>
2021-09-03 12:50:56 +0200

    rm CaloTau

--- python/ISpyCaloTau_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-ISpyCaloTau = cms.EDAnalyzer('ISpyCaloTau',
-                            iSpyCaloTauTag = cms.InputTag('caloRecoTauProducer')
-                           )

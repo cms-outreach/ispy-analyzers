4da311ca8c36cd1cd1a561f3706c53ce2b372819
mccauley <thomas.mccauley@cern.ch>
2022-04-07 17:14:58 +0200

    rm unused analyzers

--- python/ISpyBasicCluster_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-ISpyBasicCluster = cms.EDAnalyzer('ISpyBasicCluster' ,
-                                  iSpyBasicClusterTag = cms.InputTag("hybridSuperClusters:hybridBarrelBasicClusters")
-                                  )
-                                 
--- python/ISpyGsfTrack_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-ISpyGsfTrack = cms.EDAnalyzer('ISpyGsfTrack',
-                              iSpyGsfTrackTag = cms.InputTag('electronGsfTracks')
-                              )

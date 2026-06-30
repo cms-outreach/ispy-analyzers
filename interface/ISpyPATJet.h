89c722a2849718f74f10eaa2295c57f57f87d736
mccauley <thomas.mccauley@cern.ch>
2026-06-30 15:41:28 +0200

    ak4/ak8 jet fixes

--- interface/ISpyPATJet.h
+++ interface/ISpyPATJet.h
@@ -1,18 +1,28 @@
 #ifndef ANALYZER_ISPY_PATJET_H
 #define ANALYZER_ISPY_PATJET_H
 
-#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Utilities/interface/InputTag.h"
 #include "DataFormats/PatCandidates/interface/Jet.h"
 
-class ISpyPATJet : public edm::one::EDAnalyzer<>
+class IgDataStorage;
+
+class ISpyPATJet : public edm::EDAnalyzer
 {
 public:
   explicit ISpyPATJet(const edm::ParameterSet&);
   virtual ~ISpyPATJet(void){}
   virtual void analyze(const edm::Event&, const edm::EventSetup&);
+
+  void handleJetCollection(edm::Handle<std::vector<pat::Jet> >&, IgDataStorage*, edm::InputTag&, const char*);
+
 private:
   edm::InputTag inputTag_;
-  edm::EDGetTokenT<std::vector<pat::Jet> > jetToken_;
-};
+  edm::InputTag ak4InputTag_;
+  edm::InputTag ak8InputTag_;
+  
+  edm::EDGetTokenT<std::vector<pat::Jet> > jetToken_;	
+  edm::EDGetTokenT<std::vector<pat::Jet> > ak4JetToken_;	
+  edm::EDGetTokenT<std::vector<pat::Jet> > ak8JetToken_;	
+};	
 #endif // ANALYZER_ISPY_PATJET_H
--- src/ISpyPATJet.cc
+++ src/ISpyPATJet.cc
@@ -15,9 +15,55 @@ using namespace edm::service;
 using namespace edm;
 
 ISpyPATJet::ISpyPATJet(const edm::ParameterSet& iConfig)
-: inputTag_(iConfig.getParameter<edm::InputTag>("iSpyPATJetTag"))
+  : inputTag_(iConfig.getParameter<edm::InputTag>("iSpyPATJetTag")),
+    ak4InputTag_(iConfig.getParameter<edm::InputTag>("iSpyAK4PATJetTag")),
+    ak8InputTag_(iConfig.getParameter<edm::InputTag>("iSpyAK8PATJetTag"))
 {
   jetToken_ = consumes<std::vector<pat::Jet> >(inputTag_);
+  ak4JetToken_ = consumes<std::vector<pat::Jet> >(ak4InputTag_);
+  ak8JetToken_ = consumes<std::vector<pat::Jet> >(ak8InputTag_);
+}
+
+void ISpyPATJet::handleJetCollection(edm::Handle<std::vector<pat::Jet> >& collection,
+				     IgDataStorage* storage,
+				     edm::InputTag& inputTag,
+				     const char* igCollectionName)
+{  
+  std::string product = "pat::Jets "
+			+ edm::TypeID (typeid (std::vector<pat::Jet>)).friendlyClassName() + ":"
+			+ inputTag.label() + ":"
+			+ inputTag.instance() + ":"
+			+ inputTag.process();
+
+  IgCollection& products = storage->getCollection("Products_V1");
+  IgProperty PROD = products.addProperty("Product", std::string ());
+  IgCollectionItem item = products.create();
+  item[PROD] = product;
+
+  IgCollection& jets = storage->getCollection(igCollectionName);
+
+  IgProperty ET = jets.addProperty("et", 0.0); 
+  IgProperty ETA = jets.addProperty("eta", 0.0);
+  IgProperty THETA = jets.addProperty("theta", 0.0);
+  IgProperty PHI = jets.addProperty("phi", 0.0);
+    
+  IgProperty VTX = jets.addProperty("vertex", IgV3d());
+
+  for ( std::vector<pat::Jet>::const_iterator ij = collection->begin(), ije = collection->end(); 
+	ij != ije; ++ij )
+  {
+    IgCollectionItem jet = jets.create();
+
+    jet[ET]    = static_cast<double>(ij->et());
+    jet[ETA]   = static_cast<double>(ij->eta());
+    jet[THETA] = static_cast<double>(ij->theta());
+    jet[PHI]   = static_cast<double>(ij->phi());
+    
+    jet[VTX] = IgV3d(ij->vx()/100.0,
+		     ij->vy()/100.0,
+		     ij->vz()/100.0);
+
+  }
 }
 
 void ISpyPATJet::analyze(const edm::Event& event, const edm::EventSetup& eventSetup)
@@ -36,57 +82,55 @@ void ISpyPATJet::analyze(const edm::Event& event, const edm::EventSetup& eventSe
   IgDataStorage *storage = config->storage();
 
   edm::Handle<std::vector<pat::Jet> > collection;
+
   event.getByToken(jetToken_, collection);
 
   if ( collection.isValid() )
   {
-    std::string product = "pat::Jets "
-                          + edm::TypeID (typeid (std::vector<pat::Jet>)).friendlyClassName() + ":"
-                          + inputTag_.label() + ":"
-                          + inputTag_.instance() + ":"
-                          + inputTag_.process();
-
-    IgCollection& products = storage->getCollection("Products_V1");
-    IgProperty PROD = products.addProperty("Product", std::string ());
-    IgCollectionItem item = products.create();
-    item[PROD] = product;
-
-    IgCollection& jets = storage->getCollection("PATJets_V1");
-
-    IgProperty ET = jets.addProperty("et", 0.0); 
-    IgProperty ETA = jets.addProperty("eta", 0.0);
-    IgProperty THETA = jets.addProperty("theta", 0.0);
-    IgProperty PHI = jets.addProperty("phi", 0.0);
-    
-    IgProperty VTX = jets.addProperty("vertex", IgV3d());
-
-    for ( std::vector<pat::Jet>::const_iterator ij = collection->begin(), ije = collection->end(); 
-          ij != ije; ++ij )
-    {
-       IgCollectionItem jet = jets.create();
-
-       jet[ET]    = static_cast<double>(ij->et());
-       jet[ETA]   = static_cast<double>(ij->eta());
-       jet[THETA] = static_cast<double>(ij->theta());
-       jet[PHI]   = static_cast<double>(ij->phi());
-       
-       jet[VTX] = IgV3d(ij->vx()/100.0,
-                        ij->vy()/100.0,
-                        ij->vz()/100.0);
-
-     }
-   }
-
-   else
-   {
-     std::string error = "### Error: pat::Jets "
-                         + edm::TypeID (typeid (std::vector<pat::Jet>)).friendlyClassName() + ":"
-                         + inputTag_.label() + ":"
-                         + inputTag_.instance() + ":"
-                         + inputTag_.process() + " are not found.";
-     config->error(error);
+    handleJetCollection(collection, storage, inputTag_, "PATJets_V1");
+  }
+  else
+  {
+    std::string error = "### Error: pat::Jets "
+			+ edm::TypeID (typeid (std::vector<pat::Jet>)).friendlyClassName() + ":"
+			+ inputTag_.label() + ":"
+			+ inputTag_.instance() + ":"
+			+ inputTag_.process() + " are not found.";
+    config->error(error);
   }
 
+  event.getByToken(ak4JetToken_, collection);
+
+  if ( collection.isValid() )
+  {
+    handleJetCollection(collection, storage, ak4InputTag_, "AK4PATJets_V1");
+  }
+  else
+  {
+    std::string error = "### Error: pat::Jets "
+			+ edm::TypeID (typeid (std::vector<pat::Jet>)).friendlyClassName() + ":"
+			+ ak4InputTag_.label() + ":"
+			+ ak4InputTag_.instance() + ":"
+			+ ak4InputTag_.process() + " are not found.";
+    config->error(error);
+  }
+
+  
+  event.getByToken(ak8JetToken_, collection);
+
+  if ( collection.isValid() )
+  {
+    handleJetCollection(collection, storage, ak8InputTag_, "AK8PATJets_V1");
+  }
+  else
+  {
+    std::string error = "### Error: pat::Jets "
+			+ edm::TypeID (typeid (std::vector<pat::Jet>)).friendlyClassName() + ":"
+			+ ak8InputTag_.label() + ":"
+			+ ak8InputTag_.instance() + ":"
+			+ ak8InputTag_.process() + " are not found.";
+    config->error(error);
+  }  
 }
 
 DEFINE_FWK_MODULE(ISpyPATJet);
